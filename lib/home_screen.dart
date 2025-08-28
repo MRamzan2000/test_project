093501c71ee4c1dc2419b7ee27e1be77cc60a3be
MRamzan2000 <ramzanahm7450@gmail.com>
2025-08-28 16:09:19 +0500

    Home Screen Listview builder transaction History

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,14 +1,41 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:get/get.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
-
   @override
   State<HomeScreen> createState() => _HomeScreenState();
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  final RxList<Map<String, dynamic>> transactionList =
+      <Map<String, dynamic>>[
+        {
+          "image": 'assets/transaction1.png',
+          "name": "Upwork",
+          "date": "Today",
+          "amount": "+ \$ 850.00",
+        },
+        {
+          "image": 'assets/transaction2.png',
+          "name": "Transfer",
+          "date": "Yesterday",
+          "amount": "- \$ 85.00",
+        },
+        {
+          "image": 'assets/transaction3.png',
+          "name": "Paypal",
+          "date": "Jan 30, 2022",
+          "amount": "+ \$ 1,406.00",
+        },
+        {
+          "image": 'assets/transaction4.png',
+          "name": "Youtube",
+          "date": "Jan 16, 2022",
+          "amount": "- \$ 11.99",
+        },
+      ].obs;
   @override
   Widget build(BuildContext context) {
     double screenHeight = MediaQuery.sizeOf(context).height;
@@ -18,7 +45,7 @@ class _HomeScreenState extends State<HomeScreen> {
       body: Column(
         children: [
           SizedBox(
-            height: screenHeight * 0.5,
+            height: screenHeight * 0.4,
             width: screenWidth,
             child: Stack(
               children: [
@@ -36,6 +63,14 @@ class _HomeScreenState extends State<HomeScreen> {
                         screenHeight * 0.06,
                       ),
                     ),
+                    boxShadow: [
+                      BoxShadow(
+                        color: Color(0xff2A7C76).withOpacity(0.2),
+                        offset: Offset(0, 8),
+                        spreadRadius: 9,
+                        blurRadius: 14,
+                      ),
+                    ],
                     gradient: LinearGradient(
                       begin: Alignment.topLeft,
                       end: Alignment.bottomRight,
@@ -59,7 +94,7 @@ class _HomeScreenState extends State<HomeScreen> {
                                 Text(
                                   "Good afternoon,",
                                   style: TextStyle(
-                                    fontSize: screenHeight * 0.019,
+                                    fontSize: screenHeight * 0.017,
                                     color: Colors.white,
                                     fontWeight: FontWeight.w500,
                                   ),
@@ -67,7 +102,7 @@ class _HomeScreenState extends State<HomeScreen> {
                                 Text(
                                   "Enjelin Morgeana",
                                   style: TextStyle(
-                                    fontSize: screenHeight * 0.025,
+                                    fontSize: screenHeight * 0.023,
                                     color: Colors.white,
                                     fontWeight: FontWeight.w800,
                                   ),
@@ -76,7 +111,7 @@ class _HomeScreenState extends State<HomeScreen> {
                             ),
                             SvgPicture.asset(
                               "assets/notification_icons.svg",
-                              height: screenHeight * 0.05,
+                              height: screenHeight * 0.045,
                             ),
                           ],
                         ),
@@ -96,6 +131,14 @@ class _HomeScreenState extends State<HomeScreen> {
                     decoration: BoxDecoration(
                       color: Color(0xff2A7C76),
                       borderRadius: BorderRadius.circular(20),
+                      boxShadow: [
+                        BoxShadow(
+                          color: Color(0xff2A7C76).withOpacity(0.3),
+                          offset: Offset(0, 8),
+                          spreadRadius: 9,
+                          blurRadius: 14,
+                        ),
+                      ],
                     ),
                     child: Column(
                       children: [
@@ -114,7 +157,7 @@ class _HomeScreenState extends State<HomeScreen> {
                                     Text(
                                       "Total Balance",
                                       style: TextStyle(
-                                        fontSize: screenHeight * 0.022,
+                                        fontSize: screenHeight * 0.02,
                                         color: Colors.white,
                                         fontWeight: FontWeight.w800,
                                       ),
@@ -122,14 +165,14 @@ class _HomeScreenState extends State<HomeScreen> {
                                     SizedBox(width: screenWidth * 0.016),
                                     SvgPicture.asset(
                                       "assets/top_forward.svg",
-                                      height: screenHeight * 0.03,
+                                      height: screenHeight * 0.024,
                                     ),
                                   ],
                                 ),
                                 Text(
                                   "\$ 2,548.00",
                                   style: TextStyle(
-                                    fontSize: screenHeight * 0.026,
+                                    fontSize: screenHeight * 0.024,
                                     color: Colors.white,
                                     fontWeight: FontWeight.bold,
                                   ),
@@ -143,7 +186,7 @@ class _HomeScreenState extends State<HomeScreen> {
                               ),
                               child: SvgPicture.asset(
                                 "assets/three_dots.svg",
-                                height: screenHeight * 0.007,
+                                height: screenHeight * 0.006,
                               ),
                             ),
                           ],
@@ -159,11 +202,11 @@ class _HomeScreenState extends State<HomeScreen> {
                                   mainAxisAlignment: MainAxisAlignment.start,
                                   children: [
                                     SvgPicture.asset("assets/decrement.svg"),
-                                    SizedBox(width: screenWidth * 0.012),
+                                    SizedBox(width: screenWidth * 0.016),
                                     Text(
                                       "Income",
                                       style: TextStyle(
-                                        fontSize: screenHeight * 0.018,
+                                        fontSize: screenHeight * 0.02,
                                         color: Color(0xffD0E5E4),
                                         fontWeight: FontWeight.w500,
                                       ),
@@ -193,12 +236,12 @@ class _HomeScreenState extends State<HomeScreen> {
                                   mainAxisAlignment: MainAxisAlignment.start,
                                   children: [
                                     SvgPicture.asset("assets/increment.svg"),
-                                    SizedBox(width: screenWidth * 0.012),
+                                    SizedBox(width: screenWidth * 0.018),
 
                                     Text(
                                       "Expenses",
                                       style: TextStyle(
-                                        fontSize: screenHeight * 0.018,
+                                        fontSize: screenHeight * 0.02,
                                         color: Color(0xffD0E5E4),
                                         fontWeight: FontWeight.w500,
                                       ),
@@ -229,6 +272,76 @@ class _HomeScreenState extends State<HomeScreen> {
                   ),
                 ),
               ],
+            ),
+          ),
+          Expanded(
+            child: Padding(
+              padding: EdgeInsets.symmetric(horizontal: screenWidth * 0.05),
+              child: Column(
+                children: [
+                  Row(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        "Transactions history",
+                        style: TextStyle(
+                          fontSize: screenHeight * 0.022,
+                          color: Color(0xff222222),
+                          fontWeight: FontWeight.w600,
+                        ),
+                      ),
+                      Text(
+                        "See all",
+                        style: TextStyle(
+                          fontSize: screenHeight * 0.018,
+                          color: Color(0xff666666),
+                          fontWeight: FontWeight.w400,
+                        ),
+                      ),
+                    ],
+                  ),
+                  SizedBox(height: screenHeight * 0.01),
+                  ListView.builder(
+                    padding: EdgeInsets.zero,
+                      itemCount:transactionList.length,
+                      shrinkWrap: true,
+                      itemBuilder: (context,index){
+                        return
+                           ListTile(
+                             contentPadding: EdgeInsets.zero,
+                             leading:  Image.asset(transactionList[index]['image'],
+                               width: screenWidth*0.13,),
+                             title:  Text(
+                               transactionList[index]["name"],
+                               style: TextStyle(
+                                 fontSize: screenHeight * 0.02,
+                                 color: Color(0xff222222),
+                                 fontWeight: FontWeight.w600,
+                               ),
+                             ),
+                             subtitle: Text(
+                               transactionList[index]["date"],
+                               style: TextStyle(
+                                 fontSize: screenHeight * 0.016,
+                                 color: Color(0xff666666),
+                                 fontWeight: FontWeight.w400,
+                               ),
+                             ) ,
+                             trailing: Text(
+                               transactionList[index]["amount"],
+                               style: TextStyle(
+                                 fontSize: screenHeight * 0.022,
+                                 color:transactionList[index]["amount"].toString().contains("-")? Color(0xffF95B51):
+                                 Color(0xff25A969),
+                                 fontWeight: FontWeight.w600,
+                               ),
+                             ),
+
+                           );
+                      })
+                ],
+              ),
             ),
           ),
         ],
